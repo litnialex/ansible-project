150aa026db86cac2ab471ca8a20bc8c476eec457
Litni Alex <litnialex@gmail.com>
2016-07-01 12:18:03 +0300

    all at once, again.

---
+++ roles/moinmoin/templates/wikiexport.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+wiki_name=$1
+target_dir={{ moinmoin_data_dir }}/export/${wiki_name}/
+lock_dir=/var/lock/${wiki_name}
+
+if ! mkdir $lock_dir ; then
+  echo "Lock failed - exit" >&2
+  exit 1
+fi
+
+rm -rf "{{ moinmoin_data_dir }}/export/${wiki_name}"
+moin --quiet --wiki-url=${wiki_name} export dump --target-dir=${target_dir} --username=LitniAlex 2> /dev/null
+cp -a /usr/lib/python2.7/dist-packages/MoinMoin/web/static/htdocs/modern/ ${target_dir} 
+rmdir $lock_dir

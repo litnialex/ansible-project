56322248b50625f987aa019e8bd23669af352135
LitniAlex <litnialex@gmail.com>
2018-08-18 09:54:51 +0300

    daily

--- roles/mail_server/templates/roundcube/config.inc.php
+++ roles/mail_server/templates/roundcube/config.inc.php
@@ -7,11 +7,11 @@ $config['support_url'] = '';
 $config['skin_logo'] = '';
 $config['des_key'] = '{{ lookup("password", "{{ roundcube_des_key_path }} length=24" ) }}';
 $config['spellcheck_engine'] = 'pspell';
-
+$config['smtp_log'] = true;
+$config['smtp_debug'] = false;
 $config['log_logins'] = true;
 $config['log_session'] = true;
 $config['imap_debug'] = false;
-$config['smtp_debug'] = false;
 $config['reply_mode'] = 1;
 $config['language'] = '{{ roundcube_language }}';
 $config['plugins'] = array('subscriptions_option','managesieve','additional_message_headers');
@@ -19,5 +19,8 @@ $config['use_subscriptions'] = false;
 
 $config['log_driver'] = 'syslog';
 $config['syslog_facility'] = LOG_MAIL;
+//$config['debug_level'] = 4;
 
 $config['managesieve_default'] = '/etc/dovecot/sieve/default.sieve';
+$config['session_lifetime'] = 30;
+?>
